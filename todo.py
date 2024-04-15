24e5c27b95af1c080f79cc8819874d4fe2a5c369
Jean Azar <jeancarlosazar@gmail.com>
2024-04-14 21:24:03 -0400

    typo2

--- todo.py
+++ todo.py
@@ -60,11 +60,11 @@ def main():
             add_task(task)
         elif choice == "2":
             print_tasks()
-            index = int(input("Ingrese el número de la completada: ")) - 1
+            index = int(input("Ingrese el número de la que completo: ")) - 1
             mark_task_completed(index)
         elif choice == "3":
             print_tasks()
-            index = int(input("Ingrese el número de la tarea a eliminar: ")) - 1
+            index = int(input("Cual desea eliminar: ")) - 1
             delete_task(index)
         elif choice == "4":
             print_tasks()
